9550d4313c7b6d5d1c4dc55e7bc26c6897a2eec9
Kristi Gribble <kgribble4083@gmail.com>
2020-03-18 22:45:01 -0500

    Changing message

--- server.js
+++ server.js
@@ -9,9 +9,12 @@ server.use("/api/users", userRouter);
 server.use("/api/posts", postRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>See what your favorite LOTR characters are posting!</h2>`);
+  res.send(`<h2>See what LOTR characters are posting here!</h2>`);
+  
 });
-
+server.get('/', (req, res) => {
+  res.send(`To see the list add /api/users to the url`)
+})
 //custom middleware
 
 function logger(req, res, next) {
