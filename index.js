57b73b61938c44610a3ea1e7466c7cc8d632f2a3
Kristi Gribble <kgribble4083@gmail.com>
2020-03-19 20:54:36 -0500

    Fixed env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // code away!
 require("dotenv").config();
 const server = require("./server.js")
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
+//const port = 4000;
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port} ...`);
